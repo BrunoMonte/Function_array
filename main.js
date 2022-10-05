3e14d0e1fbcce8aa32c285443f3768100ec1fd80
Bruno Aguiar <bruno.aguiar@wisereducacao.com>
2022-10-05 16:31:08 -0300

    add ternary operator

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const companies = ["visa", "mastercard", "maestro"]
 
 companies.push("elo")      //push() adiciona novo item no array, de fato altera o array.
 
-const companiesAtualizada = companies.filter(companiess => {   //filter() pegar aquilo que eu quero, uma só ou várias.
-    return companiess !== "mastercard"
+const companiesAtualizada = companies.filter(companiess => {   //filter() pegar a propriedade que eu quero, uma só ou várias. E retorna um boolean(nesse caso)
+    return console.log(companiess !== "mastercard")
 })
 
 const companiesElo = companies.find((companiess) => {    //find() pega único item do array.
-    return companiess === "elo"
+    return console.log(companiess === "elo")
 })
 
-const found = companies.includes("elo")         // includes() retornar um boolean, se encontrou ou nao. E pode passar um segundo argumento, informando o apartir que posição.
+const found = companies.includes("elo",3)         // includes() retornar um boolean, se encontrou ou nao. E pode passar um segundo argumento, informando o apartir que posição.
 
 const companiesNew = [...companies , "hipercard"]            // ...spread, uma forma de atualizar o array, onde pega as propriedades existentes, e vc adciona outra propriedade.
 
@@ -24,6 +24,7 @@ companies.pop("elo")       //pop() igual ao push, ele altera o array, no caso re
 // map,reverse
 
 const otherCompanies = ["unionpay", "mir"]
+console.log(otherCompanies, "primeiro")
 
 const newCompanies = otherCompanies.map((companies) =>{       //map(),retorna transforma(altera) de todas propriedades.
     return "Third Companie"
@@ -32,9 +33,20 @@ const newCompanies = otherCompanies.map((companies) =>{       //map(),retorna tr
 const otherCompaniesReverse = otherCompanies.reverse()  //reverse(), desta forma ele retorna o array original revertido.
 const otherCompaniesReverseOne = [...otherCompanies].reverse()  //o comportamento do reverse, quando fazemos o spread no array origial, ele não alteral o mesmo, ele retorna um novo array.
 
+console.log(otherCompaniesReverse)
+console.log(otherCompaniesReverseOne)
+
 otherCompanies.forEach((companies) =>{      //forEach(), é um loop de interação, rodando uma função que eu queira fazer.
-    console.log(companies)
+   console.log(otherCompanies)
+    //console.log(companies,"esse")
 })
 
 console.log(otherCompanies)
 console.log(otherCompaniesReverseOne)
+
+const idadeMinima = 18
+const idadeCliente = 10
+
+
+// Exceção , operador ternário !
+if (idadeCliente >= idadeMinima ? console.log("cerveja") : console.log("suco"));
